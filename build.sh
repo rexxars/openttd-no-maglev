dfdba93a517583694cd6560c7829e7b02f0d73c0
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-02-07 16:01:04 -0800

    Exclude license from tarball

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ rm -rf dist && \
   cd src && \
   nmlc -o ../dist/no-maglev.grf no-maglev.nml && \
   cd .. && \
-  cp LICENSE dist/license.txt && \
   cp README.md dist/readme.txt && \
   cp CHANGELOG.md dist/changelog.txt && \
   tar -cf no-maglev.tar dist  && \
